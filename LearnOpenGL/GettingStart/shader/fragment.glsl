818c327652a4ca223a8da3c6f22a4372ce7009c1
Mist-Awei <wanwei8393@outlook.com>
2024-12-24 18:06:53 +0800

    Update fragment.glsl

--- LearnOpenGL/GettingStart/shader/fragment.glsl
+++ LearnOpenGL/GettingStart/shader/fragment.glsl
@@ -1,5 +1,4 @@
 #version 330 core
-in vec4 ourColor;
 in vec2 TexCoord;
 
 out vec4 FragColor;
@@ -10,5 +9,5 @@ uniform float mixValue;
 
 void main()
 {
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(TexCoord.x, TexCoord.y)), mixValue); // texture函数来采样纹理颜色（纹理采样器，纹理坐标）
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), mixValue); // texture函数来采样纹理颜色（纹理采样器，纹理坐标）
 }
