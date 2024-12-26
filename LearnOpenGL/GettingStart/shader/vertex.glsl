bd937d4a6c996657a8b6c557e8d0d983590ba889
Mist-Awei <wanwei8393@outlook.com>
2024-12-26 16:35:23 +0800

    Update vertex.glsl

--- LearnOpenGL/GettingStart/shader/vertex.glsl
+++ LearnOpenGL/GettingStart/shader/vertex.glsl
@@ -1,8 +1,8 @@
 #version 330 core
 layout (location = 0) in vec3 aPos; // 位置变量的属性位置值为0
-layout (location = 1) in vec2 aTexture; // 纹理坐标属性
+layout (location = 1) in vec2 aTexCoord; // 纹理坐标属性
 
-out vec2 TextureCor;
+out vec2 TexCoord;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -11,5 +11,5 @@ uniform mat4 projection;
 void main()
 {
 	gl_Position = projection * view * model * vec4(aPos, 1.0f);
-	TextureCor = aTexture;
+	TexCoord = aTexture;
 }
